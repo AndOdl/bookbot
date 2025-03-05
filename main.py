f69ec0448326d6b125b4010b842b21f152f6bc9a
AndOdl <andreodland@gmail.com>
2025-03-05 18:00:21 +0100

    Added sorting based on amount, and removed non-alphabetical characters

--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from stats import get_char_count
 def main():
     num_words = get_word_count()
     char_dict = get_char_count()
-    print(f"{num_words} words found in the document")
-    print(char_dict)
-
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for char in char_dict:
+        if char.isalpha():
+            print(f"{char}: {char_dict[char]}")
+        else:
+            pass
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -18,5 +18,9 @@ def get_char_count():
                 char_dict[char] += 1
             else:
                 char_dict.update({char: 1})
-    return char_dict
-    
+    sorted_dict = sort_dict(char_dict)
+    return sorted_dict
+
+def sort_dict(dictionary):
+    sorted_dict = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
+    return sorted_dict
